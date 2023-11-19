3c4bd88fe390d024eb449df32f2d7ed7dde0ef06
Aaron Diable <aaron.diable@gmail.com>
2023-11-19 20:01:17 +0000

    added 404

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.set('view engine', 'ejs');
 
 app.use('/', rootRouter);
 
+
+// 404 Error Handler
+
+app.use((req, res, next) => {
+   res.status(404).render('404', {requestedUrl: req.originalUrl});
+});
+
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`);
 })
